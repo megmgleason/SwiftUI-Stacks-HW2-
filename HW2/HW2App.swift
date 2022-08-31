8231fc8aafecd001b5954e2dcf1b425d7d0a226c
megmgleason <53867050+megmgleason@users.noreply.github.com>
2022-08-31 16:11:21 -0400

    Initial Commit

---
+++ HW2/HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  HW2App.swift
+//  HW2
+//
+//  Created by MEG G on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
